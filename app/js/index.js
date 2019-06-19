8bbbfd3e21e3b8a6bcf227abda280eab920655af
Huy Tran <kingbazoka@gmail.com>
2019-06-19 14:34:39 -0700

    Initial commit

---
+++ app/js/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const {shell} = require('electron');
+
+const webview = document.getElementById('webview');
+const loader = document.getElementById('loader');
+
+webview.addEventListener('dom-ready', () => {
+  webview.insertCSS(fs.readFileSync(path.join(__dirname, '/css/app.css'), 'utf8'));
+});
+
+webview.addEventListener('new-window', (event) => {
+  event.preventDefault();
+  shell.openExternal(event.url);
+});
+
+webview.addEventListener('did-start-loading', () => {
+  loader.classList.add('-active');
+});
+
+webview.addEventListener('did-stop-loading', () => {
+  loader.classList.remove('-active');
+});
+
+window.addEventListener('focus', () => webview.focus());
---
+++ main.js
@@ -0,0 +1,56 @@
+const {app, shell, Menu, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+const windowStateKeeper = require('electron-window-state');
+const {moveToApplications} = require('electron-lets-move');
+
+var isDevelopment = process.env.NODE_ENV === 'development';
+
+let mainWindow;
+win = null;
+const menuTemplate = require('./menu');
+
+function createWindow () {
+  let appMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(appMenu);
+
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1024,
+    defaultHeight: 700
+  });
+
+  win = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    titleBarStyle: 'hidden',
+    vibrancy: 'light',
+    minWidth: 800,
+    minHeight: 600,
+  });
+
+  mainWindowState.manage(win);
+
+  win.loadURL('file://' + __dirname + '/app/index.html');
+
+  win.on('closed', () => {
+    win = null
+  });
+
+  return win;
+}
+
+app.on('ready', () => {
+  mainWindow = createWindow();
+  if (!isDevelopment) {
+    moveToApplications();
+  }
+});
+
+app.on('activate', (event, hasVisibleWindows) => {
+  if (!hasVisibleWindows) {
+    createWindow();
+  }
+});
